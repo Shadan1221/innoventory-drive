65470ce2213478a022425b810cb4b796fb154ea3
Ahmad Shadan Taiyabi <99491957+Shadan1221@users.noreply.github.com>
2025-12-31 00:25:15 +0530

    Update require paths in admin_dashboard.php

--- pkg/user-management/admin_dashboard.php
+++ pkg/user-management/admin_dashboard.php
@@ -1,6 +1,6 @@
 <?php
-require_once "../session.php";
-require_once "../config.php";
+require_once "../../session.php";
+require_once "../../config.php";
 
 if ($_SESSION["role"] !== "admin") {
     header("Location: ../../index.php");
